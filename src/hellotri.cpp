0a25e970a19bb4838dd8c1575117fcc7922bc957
Graham Stubbs <wgs2113@columbia.edu>
2019-11-08 14:04:37 -0500

    working on making a triangle display

--- src/hellotri.cpp
+++ src/hellotri.cpp
@@ -17,7 +17,7 @@ int main() {
     } 
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
@@ -45,6 +45,73 @@ int main() {
 
     /* OTHER STUFF GOES HERE NEXT */
   
+    float points[] = {
+        0.0f,  0.5f,  0.0f,
+        0.5f, -0.5f,  0.0f,
+        -0.5f, -0.5f,  0.0f
+    };
+
+	// load points into GPU using Vertex Buffer Object (VBO)
+	GLuint vbo = 0;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), points, GL_STATIC_DRAW);
+
+	// Generate a Vertex Array Object for our triangle
+	GLuint vao = 0;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+    // vertex shader: describes where the 3d points should end up on the display
+    const char* vertex_shader =
+            "#version 330\n"
+            "in vec3 vp;"
+            "void main() {"
+            "  gl_Position = vec4(vp, 1.0);"
+            "}";
+
+    // fragments shader sets the color of each fragment
+    const char* fragment_shader =
+            "#version 400\n"
+            "out vec4 frag_colour;"
+            "void main() {"
+            "  frag_colour = vec4(0.5, 0.0, 0.5, 1.0);"
+            "}";
+
+    // load and compile vertex shader and fragment shader
+    GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vs, 1, &vertex_shader, NULL);
+    glCompileShader(vs);
+    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fs, 1, &fragment_shader, NULL);
+    glCompileShader(fs);
+
+    // combine shaders into GPU shader programme
+    GLuint shader_programme = glCreateProgram();
+    glAttachShader(shader_programme, fs);
+    glAttachShader(shader_programme, vs);
+    glLinkProgram(shader_programme);
+
+    // continually draw until window is closed
+    while(!glfwWindowShouldClose(window)) {
+        // wipe the drawing surface clear
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glUseProgram(shader_programme);
+        glBindVertexArray(vao);
+
+        // draw points 0-3 from the currently bound VAO with current in-use shader
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        
+        // update other events like input handling 
+        glfwPollEvents();
+        
+        // put the stuff we've been drawing onto the display
+        glfwSwapBuffers(window);
+    }
+
     // close GL context and any other GLFW resources
     glfwTerminate();
     return 0;
