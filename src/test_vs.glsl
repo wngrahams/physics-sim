ea620a7823a5e3859fb5fe9672001eb9bdb11356
wngrahams <w.grahamstubbs@gmail.com>
2019-11-10 20:41:14 -0500

    from textbook: added log functions, load shaders from outside files

--- src/test_vs.glsl
+++ src/test_vs.glsl
@@ -1,6 +1,7 @@
-#version 330
-in vec3 vp;
-void main() {
-    gl_Position = vec4(vp, 1.0);
-}
+#version 410
 
+in vec3 vertex_position;
+
+void main() {
+	gl_Position = vec4(vertex_position, 1.0);
+}
