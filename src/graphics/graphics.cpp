442d7f9412b9c43cb549e9622894db2a8d4f9a88
wngrahams <w.grahamstubbs@gmail.com>
2019-11-12 19:24:49 -0500

    camera kinda works

--- src/graphics/graphics.cpp
+++ src/graphics/graphics.cpp
@@ -15,8 +15,14 @@
 #include "graphics.h"
 #include "maths_funcs.hpp"
 
-#define FILE_SIZE_VSHADER (1024*256)
-#define FILE_SIZE_FSHADER (1024*256)
+#define FILE_SIZE_VSHADER (1024 * 256)
+#define FILE_SIZE_FSHADER (1024 * 256)
+
+#define CLIPPING_NEAR (0.1f)
+#define CLIPPING_FAR  (100.0f)
+#define FOV           (67.0f * ONE_DEG_IN_RAD)
+#define CAM_SPEED     (2000.0f)   // 2000 units per second
+#define CAM_YAW_SPEED (200000.0f)  // 200000 degrees per second
 
 
 int main(int argv, char** argc) {
@@ -39,7 +45,11 @@ int main(int argv, char** argc) {
 	GLuint vs, fs, shader_program;
 	const GLchar *p;
 	int params = -1;
-	GLint color_loc;
+	//GLint color_loc;
+
+    // camera vars:
+    float aspect, inverse_range, s_x, s_y, s_z, p_z, cam_yaw;
+    
 
     // restart log file:
     restart_gl_log();
@@ -132,13 +142,46 @@ int main(int argv, char** argc) {
     // END TODO
 
     GLfloat matrix[] = {
-		1.0f, 0.0f, 0.0f, 0.0f, // first column
-		0.0f, 1.0f, 0.0f, 0.0f, // second column
-		0.0f, 0.0f, 1.0f, 0.0f, // third column
-		0.5f, 0.0f, 0.0f, 1.0f	// fourth column
+		1.5f, 0.0f, 0.0f, 0.0f, // first column
+		0.0f, 1.5f, 0.0f, 0.0f, // second column
+		0.0f, 0.0f, 1.5f, 0.0f, // third column
+		0.0f, 0.0f, 0.0f, 1.0f	// fourth column
 	};
 
-    int matrix_location = glGetUniformLocation(shader_program, "matrix");
+    //int matrix_location = glGetUniformLocation(shader_program, "matrix");
+
+    /*--------------------------create camera
+	 * matrices----------------------------*/
+	/* create PROJECTION MATRIX */
+	// input variables
+	aspect = (float)g_gl_width / (float)g_gl_height; // aspect ratio
+	// matrix components
+	inverse_range = 1.0f/ tan(FOV*0.5f);
+	s_x = inverse_range/aspect;
+	s_y = inverse_range;
+	s_z = -(CLIPPING_FAR + CLIPPING_NEAR)/(CLIPPING_FAR - CLIPPING_NEAR);
+	p_z = -(2.0f * CLIPPING_FAR * CLIPPING_NEAR)/(CLIPPING_FAR - CLIPPING_NEAR);
+	GLfloat proj_mat[] = {   s_x,  0.0f,  0.0f,  0.0f,	
+                            0.0f,   s_y,  0.0f,  0.0f,
+						    0.0f,  0.0f,   s_z, -1.0f, 
+                            0.0f,  0.0f,   p_z,  0.0f };
+
+    /* create VIEW MATRIX */
+	float cam_pos[] = { 0.0f, 0.0f, 2.0f };  // don't start at zero 
+                                             // or we will be too close
+	cam_yaw = 0.0f;				// y-rotation in degrees
+	mat4 T =
+		translate( identity_mat4(), vec3( -cam_pos[0], -cam_pos[1], -cam_pos[2] ) );
+	mat4 R = rotate_y_deg( identity_mat4(), -cam_yaw );
+	mat4 view_mat = R * T;
+
+	// get location numbers of matrices in shader programme 
+	GLint view_mat_location = glGetUniformLocation(shader_program, "view" );
+	GLint proj_mat_location = glGetUniformLocation(shader_program, "proj" );
+	/* use program (make current in state machine) and set default matrix values*/
+	/*glUseProgram( shader_programme );
+	glUniformMatrix4fv( view_mat_location, 1, GL_FALSE, view_mat.m );
+	glUniformMatrix4fv( proj_mat_location, 1, GL_FALSE, proj_mat );*/
 
     //TODO comment this out
     print_all_shader_info(shader_program);
@@ -152,7 +195,9 @@ int main(int argv, char** argc) {
 
     // switch to the shader program:
     glUseProgram(shader_program);
-    glUniformMatrix4fv(matrix_location, 1, GL_FALSE, matrix);
+    //glUniformMatrix4fv(matrix_location, 1, GL_FALSE, matrix);
+    glUniformMatrix4fv(view_mat_location, 1, GL_FALSE, view_mat.m);
+	glUniformMatrix4fv(proj_mat_location, 1, GL_FALSE, proj_mat);
 
     // assign initial color to fragment shader:
     //glUniform4f(color_loc, 0.6f, 0.0f, 0.6f, 1.0f);
@@ -160,12 +205,14 @@ int main(int argv, char** argc) {
     // set background color:
     glClearColor(0.7f, 0.7f, 0.7f, 1.0f);  // grey
 
-    glEnable(GL_CULL_FACE);  // cull face
-	glCullFace(GL_BACK);	   // cull back face
+    //glEnable(GL_CULL_FACE);  // cull face
+	//glCullFace(GL_BACK);	   // cull back face
 	glFrontFace(GL_CW);      // GL_CCW for counter clock-wise
 
-    float speed = 10.0f;
-    float last_position = 0.0f;
+    float speed = 400.0f;
+    float last_position_x = 0.0f;
+    float last_position_y = 0.0f;
+    float last_position_z = 0.0f;
     // continually draw until window is closed 
     while (!glfwWindowShouldClose(g_window)) {
 
@@ -189,14 +236,20 @@ int main(int argv, char** argc) {
 
         // update the matrix
 		// - you could simplify this by just using sin(current_seconds)
-		matrix[12] = elapsed_seconds * speed + last_position;
-		last_position = matrix[12];
-		if ( fabs( last_position ) > 1.0 ) {
+        /*
+		matrix[0] = elapsed_seconds * speed + last_position_x;
+    	matrix[5] = elapsed_seconds * speed + last_position_y;
+	    matrix[10] = elapsed_seconds * speed + last_position_z;
+
+		last_position_x = matrix[0];
+        last_position_y = matrix[5];
+        last_position_z = matrix[10];
+		if ( fabs( last_position_x ) > 4.0 ) {
 			speed = -speed;
 		}
 		//
 		// Note: this call is related to the most recently 'used' shader programme
-		glUniformMatrix4fv( matrix_location, 1, GL_FALSE, matrix );
+		glUniformMatrix4fv( matrix_location, 1, GL_FALSE, matrix );*/
 
         // bind VAO:
         glBindVertexArray(vao);
@@ -207,6 +260,53 @@ int main(int argv, char** argc) {
 
         // update other events (i.e. user inputs);
         glfwPollEvents();
+
+        /*-----------------------------move camera
+		 * here-------------------------------*/
+		// control keys
+		bool cam_moved = false;
+		if ( glfwGetKey( g_window, GLFW_KEY_A ) ) {
+			cam_pos[0] -= CAM_SPEED * elapsed_seconds;
+			cam_moved = true;
+		}
+		if ( glfwGetKey( g_window, GLFW_KEY_D ) ) {
+			cam_pos[0] += CAM_SPEED * elapsed_seconds;
+			cam_moved = true;
+		}
+		if ( glfwGetKey( g_window, GLFW_KEY_UP ) ) {
+			cam_pos[1] += CAM_SPEED * elapsed_seconds;
+			cam_moved = true;
+		}
+		if ( glfwGetKey( g_window, GLFW_KEY_DOWN ) ) {
+			cam_pos[1] -= CAM_SPEED * elapsed_seconds;
+			cam_moved = true;
+		}
+		if ( glfwGetKey( g_window, GLFW_KEY_W ) ) {
+			cam_pos[2] -= CAM_SPEED * elapsed_seconds;
+			cam_moved = true;
+		}
+		if ( glfwGetKey( g_window, GLFW_KEY_S ) ) {
+			cam_pos[2] += CAM_SPEED * elapsed_seconds;
+			cam_moved = true;
+		}
+		if ( glfwGetKey( g_window, GLFW_KEY_LEFT ) ) {
+			cam_yaw += CAM_YAW_SPEED * elapsed_seconds;
+			cam_moved = true;
+		}
+		if ( glfwGetKey( g_window, GLFW_KEY_RIGHT ) ) {
+			cam_yaw -= CAM_YAW_SPEED * elapsed_seconds;
+			cam_moved = true;
+		}
+		/* update view matrix */
+		if (cam_moved) {
+            printf("cam moved\n");
+			mat4 T = translate(identity_mat4(), vec3( -cam_pos[0], -cam_pos[1],
+																								 -cam_pos[2] ) ); // cam translation
+			mat4 R = rotate_y_deg( identity_mat4(), -cam_yaw );					//
+			mat4 view_mat = R * T;
+			glUniformMatrix4fv(view_mat_location, 1, GL_FALSE, view_mat.m);
+		}
+
         if (GLFW_PRESS == glfwGetKey(g_window, GLFW_KEY_ESCAPE)) {
             glfwSetWindowShouldClose(g_window, 1);  // close window on esc press
         }
--- src/graphics/test_vs.glsl
+++ src/graphics/test_vs.glsl
@@ -3,11 +3,11 @@
 layout(location = 0) in vec3 vertex_position;
 layout(location = 1) in vec3 vertex_color;
 
-uniform mat4 matrix;
+uniform mat4 view, proj;
 
 out vec3 color;
 
 void main() {
     color = vertex_color;
-	gl_Position = matrix * vec4(vertex_position, 1.0);
+	gl_Position = proj * view * vec4(vertex_position, 1.0);
 }
